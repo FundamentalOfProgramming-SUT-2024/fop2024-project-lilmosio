e7c627ff0692de19e33293b236b402f513006d3a
Mostafa Naderloo <mostafanaderloo26@gmail.com>
2025-02-01 17:36:36 +0330

    Added main file

---
+++ main.c
@@ -0,0 +1,51 @@
+// main.c
+#include "game.h"
+#include "menu.h"
+#include "save.h"
+
+User users[MAX_USERS];
+int user_count = 0;
+User current_user = {0};
+
+int main() {
+    init_ncurses();
+    load_users(users, &user_count);
+
+    int choice;
+    do {
+        choice = show_main_menu();
+        switch (choice) {
+            case 1: handle_new_user(users, &user_count); break;
+            case 2: handle_login(users, user_count, &current_user); break;
+            case 3: 
+                if (current_user.username[0] != '\0') {
+                    start_new_game(&current_user);
+                } else {
+                    printw("Please login first!\n");
+                    refresh();
+                    getch();
+                }
+                break;
+            case 4: 
+                if (current_user.username[0] != '\0') {
+                    continue_saved_game(&current_user);
+                } else {
+                    printw("Please login first!\n");
+                    refresh();
+                    getch();
+                }
+                break;
+            case 5: show_leaderboard(users, user_count); break;
+            case 6: show_settings(); break;
+            case 7: break;
+            default:
+                printw("Invalid choice!\n");
+                refresh();
+                getch();
+        }
+    } while (choice != 7);
+
+    save_users(users, user_count);
+    cleanup_ncurses();
+    return 0;
+}
