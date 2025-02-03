9f4ca906553c8a0cc36a15de550ed77393ed20c1
Mostafa Naderloo <mostafanaderloo26@gmail.com>
2025-02-04 01:25:58 +0330

    Update game.c

--- game.c
+++ game.c
@@ -4,15 +4,22 @@
 
 void init_ncurses() {
     initscr();
-    cbreak();
-    noecho();
+    start_color();
+    use_default_colors();
+    
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);  
+    init_pair(2, COLOR_BLACK, COLOR_WHITE);
+    init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(4, COLOR_RED, COLOR_BLACK); 
+    init_pair(5, COLOR_CYAN, COLOR_BLACK);  
+    init_pair(6, COLOR_GREEN, COLOR_BLACK);   
+    init_pair(7, COLOR_BLUE, COLOR_BLACK);   
+    init_pair(8, COLOR_MAGENTA, COLOR_BLACK);
+    
+    bkgd(COLOR_PAIR(1));
     keypad(stdscr, TRUE);
     curs_set(0);
-    start_color();
-    
-    init_pair(1, COLOR_WHITE, COLOR_BLACK);
-    init_pair(2, COLOR_GREEN, COLOR_BLACK);
-    init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+    noecho();
 }
 
 void cleanup_ncurses() {
